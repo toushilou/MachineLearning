bd881476034069334e3c32f4370ec294e3144971
toushilou <seekyq@gmail.com>
2015-04-05 11:30:31 +0800

    chapter_1

--- Numpy/chapter_1.py
+++ Numpy/chapter_1.py
@@ -1,2 +1,14 @@
 __author__ = 'qyuan'
 
+import numpy as np
+import timeit
+
+normal_py_sec = timeit.timeit('sum(x*x for x in xrange(1000))', number=10000)
+naive_np_sec = timeit.timeit('sum(na*na)', setup='import numpy as np; na = np.arange(1000)',number=10000)
+good_np_sec = timeit.timeit('na.dot(na)', setup='import numpy as np; na = np.arange(1000)', number=10000)
+
+print normal_py_sec
+print naive_np_sec
+print good_np_sec
+a = np.array([0, 1, 2, 3, 4, 5])
+print a
