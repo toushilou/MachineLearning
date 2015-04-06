f5ad2656670c7a0e6fc7899b465d77ada07c3fd9
toushilou <seekyq@gmail.com>
2015-04-06 21:57:26 +0800

    restructure

--- chapter2/iris_data.py
+++ chapter2/iris_data.py
@@ -8,16 +8,23 @@ data = load_iris()
 features = data['data']
 feature_names = data['feature_names']
 target = data['target']
-print type(features)
-# print data
+print type(data)
+print dir(data)
+target_names = data.target_names
+print features
+
+labels = target_names[target]
 for t, marker, c in zip(xrange(3), ">ox", "rgb"):
-    print features[target == t]
     # print features[target == t, 1]
     plt.scatter(features[target == t, 0],
                 features[target == t, 1],
                 marker=marker,
                 c=c)
 
+plength = features[:, 2]
+print type(plength)
+is_setosa =  (labels == 'setosa')
+print is_setosa
 plt.show()
 
 
