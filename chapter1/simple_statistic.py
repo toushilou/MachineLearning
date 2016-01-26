60b41b9da25981e6214276c28d9f7caba326c1d0
toushilou <seekyq@gmail.com>
2016-01-26 22:31:55 +0800

    test commit

--- chapter1/simple_statistic.py
+++ chapter1/simple_statistic.py
@@ -2,10 +2,11 @@ __author__ = 'sweety'
 
 import scipy as sp
 import matplotlib.pyplot as plt
+import numpy as np
 
 data = sp.genfromtxt("../data/web_traffic.tsv", delimiter='\t')
-print data[:10]
-print data.shape
+#print data[:10]
+#print data.shape
 x = data[:, 0]
 y = data[:, 1]
 
@@ -13,20 +14,24 @@ y = data[:, 1]
 x = x[~sp.isnan(y)]
 y = y[~sp.isnan(y)]
 
-fp1, residuals, rank, sv, rcond = sp.polyfit(x, y, 50, full = True)
-print fp1
-print(residuals)
+fp1, residuals, rank, sv, rcond = sp.polyfit(x, y, 2, full = True)
+#print type(fp1)
+# print residuals
+# print rank
+# print sv
+# print rcond
 
 def error(f, x, y):
     return sp.sum((f(x) - y) ** 2)
 
-
+print fp1
 f1 = sp.poly1d(fp1)
+print f1
 print error(f1, x, y)
 
 fx = sp.linspace(0, x[-1], 1000)
 
-print fx
+#print fx
 plt.plot(fx, f1(fx), linewidth=4)
 plt.legend(['d=%i'% f1.order], loc='upper left')
 plt.scatter(x, y)
@@ -38,3 +43,6 @@ plt.autoscale(tight=True)
 plt.grid(0)
 plt.show()
 
+#t = np.arange(0., 5, 0.2)
+
+#print t
--- chapter2/iris_data.py
+++ chapter2/iris_data.py
@@ -8,23 +8,26 @@ data = load_iris()
 features = data['data']
 feature_names = data['feature_names']
 target = data['target']
-print type(data)
-print dir(data)
+#print target
+#print type(data)
+#print dir(data)
 target_names = data.target_names
-print features
-
+#print feature_names
+#print features
+#print features[0,0]
 labels = target_names[target]
+# print features
+
 for t, marker, c in zip(xrange(3), ">ox", "rgb"):
-    # print features[target == t, 1]
     plt.scatter(features[target == t, 0],
                 features[target == t, 1],
                 marker=marker,
                 c=c)
 
 plength = features[:, 2]
-print type(plength)
+#print type(plength)
 is_setosa =  (labels == 'setosa')
-print is_setosa
+#print is_setosa
 plt.show()
 
 
